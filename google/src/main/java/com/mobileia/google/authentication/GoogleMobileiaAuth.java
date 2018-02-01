e9d6c1cfcce3b7e83c4ec74057e3bbc1580e74b5
Matias Camiletti <matias.camiletti@gmail.com>
2018-02-01 10:32:02 -0300

    Agregada funcionalidad para integrar el login con Google en Mobileia Auth.

---
+++ google/src/main/java/com/mobileia/google/authentication/GoogleMobileiaAuth.java
@@ -0,0 +1,109 @@
+package com.mobileia.google.authentication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.gson.JsonObject;
+import com.mobileia.authentication.core.MobileiaAuthBase;
+import com.mobileia.authentication.core.listener.LoginResult;
+import com.mobileia.authentication.core.rest.AuthRestBase;
+import com.mobileia.core.entity.Error;
+import com.mobileia.google.MobileiaGoogle;
+import com.mobileia.google.builder.GoogleLoginBuilder;
+import com.mobileia.google.listener.OnErrorGoogleLogin;
+import com.mobileia.google.listener.OnSuccessGoogleLogin;
+
+/**
+ * Created by matiascamiletti on 1/2/18.
+ */
+
+public class GoogleMobileiaAuth extends MobileiaAuthBase {
+    /**
+     * Almacena la actividad
+     */
+    protected FragmentActivity mActivity;
+    /**
+     * Almacenamos el callback
+     */
+    protected LoginResult mCallback;
+    /**
+     * Almacena el ID de google
+     */
+    protected String mGoogleId;
+    /**
+     * Almacena el Token de google
+     */
+    protected String mGoogleToken;
+    /**
+     * Almacena instancia de MobileiaGoogle
+     */
+    protected MobileiaGoogle mMobileiaGoogle;
+
+    /**
+     * Constructor
+     *
+     * @param activity
+     */
+    public GoogleMobileiaAuth(Activity activity, String googleId) {
+        super(activity);
+        // Guardamos ID de google
+        mGoogleId = googleId;
+    }
+
+    @Override
+    public void requestAccessToken() {
+        // Generamos objeto para enviar los parametros
+        JsonObject params = new JsonObject();
+        params.addProperty("grant_type", "google");
+        params.addProperty("google_token", mGoogleToken);
+        new AuthRestBase().oauth(params, mAccessTokenResult);
+    }
+
+    @Override
+    public void requestNewAccount() {
+        // Generamos objeto para enviar los parametros
+        JsonObject params = new JsonObject();
+        params.addProperty("register_type", "google");
+        params.addProperty("google_token", mGoogleToken);
+        new AuthRestBase().register(params, mRegisterResult);
+    }
+
+    @Override
+    public void openSocial() {
+        mMobileiaGoogle = new GoogleLoginBuilder()
+                .withActivity(mActivity)
+                .withGoogleId(mGoogleId)
+                .withSuccessResult(new OnSuccessGoogleLogin() {
+                    @Override
+                    public void onSuccess(GoogleSignInAccount account) {
+                        // Almacenamos el token de google
+                        mGoogleToken = account.getIdToken();
+                        // Ya se logueo, realizamos peticion para generar AccessToken
+                        requestAccessToken();
+                    }
+                })
+                .withErrorResult(new OnErrorGoogleLogin() {
+                    @Override
+                    public void onError(int code, String message) {
+                        // Llamamos al callback con error
+                        mCallback.onError(new Error(code, message));
+                    }
+                })
+                .build();
+        mMobileiaGoogle.login();
+    }
+
+    /**
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(mMobileiaGoogle != null){
+            mMobileiaGoogle.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
